cd5f1e58d52ac81b5f3b9f248269b665d1426205
Muralidhar302 <muralidhar302@gmail.com>
2025-08-24 05:42:35 +0530

    update

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,10 +1,13 @@
-def call (map,config) {
-    Branch = config.branch
-    url = config.url
-    credentialsId = config.credentialsId
-       checkout scmGit(branches: [[name: 'config.Branch']],
-       userRemoteConfigs: [[credentialsId: 'config.credentialsId', 
-        url: 'config.url']])
+def call (map config) {
+    echo "Branch = ${config.branch}"
+    echo "url = ${config.url}"
+    credentialsId = ${config.credentialsId}
+        checkout scmGit(
+                        branches: [[name: 'config.Branch']],
+                        userRemoteConfigs:[[credentialsId: 'config.credentialsId', 
+                                            url: 'config.url'
+                        ]]
+        )
 
 }
 
@@ -12,3 +15,4 @@ def call (map,config) {
     // Branch: 'main',
     // credentialsId: 'github_token', 
     // url: 'https://github.com/Muralidhar302/jenkins_shared_libraries.git'
+
